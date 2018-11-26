062038143ec9392a1ad6aa1bd83c989fa06cf512
Samuel Hurel <samuel.hurel@gmail.com>
2018-11-26 12:24:44 +0100

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/mtc/precache-manifest.61b535e18cdd907c0c4ae08ffd99fb7a.js"
+  "/mtc/precache-manifest.700e23c527db2bccf7a11e08579664b0.js"
 );
 
 workbox.clientsClaim();
---
+++ static/js/pages-PricingPage.84b78b16.chunk.js
@@ -0,0 +1,2 @@
+(window.__LOADABLE_LOADED_CHUNKS__=window.__LOADABLE_LOADED_CHUNKS__||[]).push([[3],{55:function(e,n,t){"use strict";t.r(n),t.d(n,"default",function(){return u});var _=t(0),r=t.n(_),a=t(7);function u(){return r.a.createElement(a.a,{padded:!0},"Hello ",r.a.createElement("code",null,"PricingPage"),"!")}}}]);
+//# sourceMappingURL=pages-PricingPage.84b78b16.chunk.js.map
