732d69ffa4f78413d3c57bbfc772cf67b31e0789
Samuel Hurel <samuel.hurel@gmail.com>
2021-05-26 11:20:56 +0200

    Use GA4 tracking ID

--- src/lib/gtag.js
+++ src/lib/gtag.js
@@ -1,4 +1,5 @@
-export const GA_TRACKING_ID = "UA-92413001-1";
+// export const GA_TRACKING_ID = "UA-92413001-1";
+export const GA_TRACKING_ID = "G-M8R1GVGHFW";
 
 // https://developers.google.com/analytics/devguides/collection/gtagjs/pages
 export const pageview = (url) => {
